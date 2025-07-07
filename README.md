42171ca24d36a076d84a4501fdcf532c72a0d053
TSUYUSATO Kitsune <make.just.on@gmail.com>
2025-07-07 14:49:40 +0900

    Add the implementation

---
+++ README.md
@@ -0,0 +1,16 @@
+# clock by [@makenowjust][]
+
+> A simple clock that displays the current time in a unique way, inspired by [Humans Since 1982](https://www.youtube.com/watch?v=IA4UsV04bnM).
+
+![a screenshot of this app](screenshot.png)
+
+Also, this project is inspired by the [@akira888](https://github.com/akira888)'s [talk](https://speakerdeck.com/akira888/hutuunoji-shu-sutatukudeatozuo-pin-wozuo-tutemiru) at [Kansai RubyKaigi 08](https://regional.rubykaigi.org/kansai08/).
+
+## License
+
+This project is licensed under the CC-BY 4.0 license.
+See [LICENSE](LICENSE) for details.
+
+(C) 2025 Hiroya Fujinami (a.k.a. [@makenowjust][])
+
+[@makenowjust]: https://github.com/makenowjust
---
+++ src/main.ts
@@ -0,0 +1,42 @@
+// This file is the main entry point for the clock application.
+
+import { create, setNextRotate, MODES, type ClockComponent } from "./clock";
+
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const container = document.createElement("container");
+container.className = "container";
+
+const clocks: ClockComponent[] = [];
+
+for (let y = 0; y < 8; y++) {
+  const row = document.createElement("div");
+  row.className = "row";
+
+  for (let x = 0; x < 15; x++) {
+    const clock = create(x, y);
+    clocks.push(clock);
+    clock.elem.style.setProperty("--clock-big-hand-rotate", `${Math.random() * 360}deg`);
+    clock.elem.style.setProperty("--clock-small-hand-rotate", `${Math.random() * 360}deg`);
+    row.appendChild(clock.elem);
+  }
+
+  container.appendChild(row);
+}
+
+app.appendChild(container);
+
+let modeIndex = 1; // Start with the "digital" mode.
+
+const updateClocks = () => {
+  const nextDate = new Date(Date.now() + 1000 * 30);
+  for (const clock of clocks) {
+    setNextRotate(nextDate, clock, MODES[modeIndex]);
+  }
+  modeIndex = (modeIndex + 1) % MODES.length;
+};
+
+setTimeout(updateClocks, 0);
+setInterval(updateClocks, 1000*30);
